05cf67ff93627af2485c0a61da8bf2059e5695e8
Teeratas Thiangtham <teerataskiwi@gmail.com>
2026-02-28 14:11:45 +0700

    feat: Add player animation and hurtbox

--- level.gd
+++ level.gd
@@ -1,6 +1,15 @@
 extends Node2D
 
 @export var clear_color := Color("#A3B3FE")
+@onready var player: Player = $Player
 
 func _ready() -> void:
 	RenderingServer.set_default_clear_color(clear_color)
+	player.died.connect(_on_player_died)
+
+
+func _on_player_died() -> void:
+	get_tree().paused = true
+	await get_tree().create_timer(5.0).timeout
+	get_tree().paused = false
+	get_tree().reload_current_scene()
--- player.gd
+++ player.gd
@@ -8,22 +8,50 @@ extends CharacterBody2D
 @export var jump_height := 4.5
 
 @onready var gravity := ProjectSettings.get_setting("physics/2d/default_gravity") as float
+@onready var animation: AnimatedSprite2D = $AnimatedSprite2D
+@onready var hurtbox: Area2D = $HurtArea
+
+signal died
 
 var jump_init_velocity: float:
 	get():
 		return sqrt(2 * jump_height * tile_size * gravity)
 
+
+func _ready() -> void:
+	animation.play("default")
+	hurtbox.area_entered.connect(func(_area): die())
+
+
 func _physics_process(delta: float) -> void:
 	velocity.y += gravity * delta
 
-	if Input.is_action_pressed("ui_right"):
+	if Input.is_action_pressed("run_right"):
 		velocity.x = speed * tile_size
-	elif Input.is_action_pressed("ui_left"):
+		animation.flip_h = false
+	elif Input.is_action_pressed("run_left"):
 		velocity.x = -speed * tile_size
+		animation.flip_h = true
 	else:
 		velocity.x = 0.0
 
-	if Input.is_action_just_pressed("ui_up") and is_on_floor():
+	if Input.is_action_pressed("jump") and is_on_floor():
 		velocity.y = -jump_init_velocity
 
+	if is_on_floor():
+		if velocity.x == 0.0:
+			animation.play("default")
+		else:
+			animation.play("run")
+	else:
+		if velocity.y < 0.0:
+			animation.play("jump")
+		else:
+			animation.play("default")
+
 	move_and_slide()
+
+
+func die() -> void:
+	animation.play("die")
+	died.emit()
